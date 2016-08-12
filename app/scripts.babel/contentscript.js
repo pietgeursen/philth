70f745b00745b92d0592ca2834473082608372a2
Piet Geursen <pietgeursen@gmail.com>
2016-08-12 16:44:28 +1200

    good enuph

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -1,3 +1,4 @@
-'use strict';
-
-console.log('\'Allo \'Allo! Content script');
+var textNode, walk=document.createTreeWalker(document,NodeFilter.SHOW_TEXT,null,false);
+while(textNode=walk.nextNode()) {
+    textNode.nodeValue = textNode.nodeValue.replace(/f/g, 'ph').replace(/F/g, 'Ph');
+}
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -119,7 +119,7 @@ gulp.task('wiredep', () => {
 gulp.task('package', function () {
   var manifest = require('./dist/manifest.json');
   return gulp.src('dist/**')
-      .pipe($.zip('phull phlaps-' + manifest.version + '.zip'))
+      .pipe($.zip('philth-' + manifest.version + '.zip'))
       .pipe(gulp.dest('package'));
 });
 
